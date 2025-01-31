368c10654ca2e32c74c6ea5cb518f233fbc58e51
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-01-31 14:45:54 -0300

    Update game.gd

--- game.gd
+++ game.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-var peer = ENetMultiplayerPeer.new() 
-const PORT = 3333
+var peer = ENetMultiplayerPeer.new()
 const ADDRESS = "127.0.0.1"
+const PORT =  3333
 
+#Exibir mensagem quando o servidor for criado e exibir mensagens sempre que
+#um usuário se conectar
+#Esconder menu de Multiplayer ao se conectar ou criar servidor com sucesso
 func _on_botao_join_pressed() -> void:
 	peer.create_client(ADDRESS, PORT)
 	multiplayer.multiplayer_peer = peer
