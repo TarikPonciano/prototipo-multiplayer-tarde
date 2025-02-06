8b0948c56a79557e991b54234b8ebf6bd379800e
Tarik Ponciano <tarikponciano@gmail.com>
2025-02-06 14:35:59 -0300

    Adicionar Jogador

--- game.gd
+++ game.gd
@@ -5,6 +5,7 @@ const ADDRESS = "127.0.0.1"
 const PORT =  3333
 @onready var log = $Log
 @onready var ui = $MultiplayerUI
+@export var playerScene : PackedScene
 
 #Exibir mensagem quando o servidor for criado e exibir mensagens sempre que
 #um usuário se conectar
@@ -32,6 +33,7 @@ func _on_botao_host_pressed() -> void:
 		log.text += "Servidor criado na porta "+str(PORT)+"!\n"
 		ui.visible = false
 		#criar personagem
+		adicionar_jogador(multiplayer.get_unique_id())
 	
 	else:
 		log.text += "Erro ao criar servidor! Código do erro: "+str(resultado) +"\n"
@@ -42,6 +44,12 @@ func player_conectado(id_jogador):
 	log.text += "Cliente "+str(id_jogador)+" conectado ao servidor!\n"
 	#Chamada rpc aqui
 	rpc("atualizar_log", log.text)
+	
+func adicionar_jogador(id_jogador):
+	var novo_player = playerScene.instantiate()
+	novo_player.name = str(id_jogador)
+	add_child(novo_player)
+	
 	
 #Criar a função atualizar_log em que recebe o log.text do servidor e modifica o próprio log
 @rpc("any_peer", "call_local", "reliable")
